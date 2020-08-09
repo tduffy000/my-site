f8bbbf1d222de41272fc285547367cc602016dff
Thomas Duffy <tduffy000@citymail.cuny.edu>
2020-08-09 13:17:33 -0400

    http global forwarding rule

--- terraform/gcp/load_balancer/main.tf
+++ terraform/gcp/load_balancer/main.tf
@@ -1,3 +1,4 @@
+# https://www.terraform.io/docs/providers/google/r/compute_ssl_certificate.html
 resource "google_compute_target_http_proxy" "main" {
   name    = "my-site-http-proxy"
   url_map = google_compute_url_map.bucket.id
@@ -13,7 +14,7 @@ resource "google_compute_url_map" "bucket" {
   }
 
   path_matcher {
-    name = "static-bucket"
+    name            = "static-bucket"
     default_service = google_compute_backend_bucket.site.id
 
     path_rule {
@@ -27,4 +28,10 @@ resource "google_compute_backend_bucket" "site" {
   name        = "static-bucket"
   bucket_name = var.bucket_name
   enable_cdn  = false
+}
+
+resource "google_compute_global_forwarding_rule" "http" {
+  name                = "http-rule"
+  target              = google_compute_target_http_proxy.main.id
+  port_range          = 80
 }
---
+++ terraform/gcp/load_balancer/outputs.tf
@@ -0,0 +1,39 @@
+output "http_proxy_id" {
+    value = google_compute_target_http_proxy.main.id
+}
+
+output "http_proxy_proxy_id" {
+    value = google_compute_target_http_proxy.main.proxy_id
+}
+
+output "http_proxy_self_link" {
+    value = google_compute_target_http_proxy.main.self_link
+}
+
+output "url_map_id" {
+    value = google_compute_url_map.bucket.id
+}
+
+output "url_map_map_id" {
+    value = google_compute_url_map.bucket.map_id
+}
+
+output "url_map_self_link" {
+    value = google_compute_url_map.bucket.self_link
+}
+
+output "bucket_backend_id" {
+    value = google_compute_backend_bucket.site.id
+}
+
+output "bucket_backend_self_link" {
+    value = google_compute_backend_bucket.site.self_link
+}
+
+output "http_global_forwarding_rule_id" {
+    value = google_compute_global_forwarding_rule.http.id
+}
+
+output "http_global_forwarding_rule_self_link" {
+    value = google_compute_global_forwarding_rule.http.self_link
+}
